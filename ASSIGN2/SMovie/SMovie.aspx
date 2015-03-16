22bf420aed4d93b7fff80d3d376013242fbff735
spitrola <spitrola27@gmail.com>
2015-03-16 01:06:38 -0600

    SMovieALMOST

--- ASSIGN2/SMovie/SMovie.aspx
+++ ASSIGN2/SMovie/SMovie.aspx
@@ -2,7 +2,8 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <style>
-        .row {margin-top:5%;}
+        .rowCol {margin-top:1.5%;}
+        tr,td {text-align:center; }
     </style>
     <h2><%: Title %>.</h2>
     <div class="row">
@@ -10,19 +11,20 @@
             <div class="col-md-4">
                 <figure>
                     <!--ADD THE RIGHT IMAGE HERE-->
-                    <img src="/images/Computer-Cat.jpg" alt="..." class="thumbnail" width="300px"/>
+                    <!--<img src="" alt="..." class="thumbnail" />-->
+                    <asp:Image runat="server" ID="imgPoster" class="thumbnail" />
                 </figure>
             </div>
 
             <!--Movie Information-->
-            <div class="col-md-8">
+            <div class="col-md-8 rowCol">
                 <!--This is the important information for a movie-->
                 <div class="panel panel-default">
                     <div class="panel-heading"><asp:Label ID="txtTitle" runat="server" /> (<asp:Label ID="txtReleaseDate" runat="server" />)</div>
                     <div class="panel-body">
-                        <p>Rating: <asp:Label ID="txtRating" runat="server" /></p>
                         <p>Run Time: <asp:Label ID="txtRunTime" runat="server" /></p>
                         <p>Genre: <asp:Label ID="txtGenre" runat="server" /></p>
+                        <p>Companies: <asp:Label ID="txtCompany" runat="server" /></p>
                     </div>
                 </div>
                 <!--This is the movie overview-->
@@ -42,26 +44,25 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Key Words</div>
                     <div class="panel-body">
-                        <p>Add Key words</p>
+                        <p><asp:Label ID="txtKeyword" runat="server" /></p>
                     </div>
                 </div>
             </div>
         </div>
         <!-- Side bar content -->
-        <div class="col-md-2">
+        <div class="col-md-2 rowCol">
             <div class="panel panel-default">
-                <div class="panel-heading">Movie Details</div>
+                <div class="panel-heading">Details</div>
                 <div class="panel-body">
-                <p>Average Rating: <asp:Label ID="txtAverage" runat="server" /></p>
+                <p>Vote Average: <asp:Label ID="txtAverage" runat="server" /></p>
                 <p>Vote Count: <asp:Label ID="txtVoteCount" runat="server" /></p>
                 </div>
             </div>
 
             <div class="panel panel-default">
-                <div class="panel-heading">Movie Information</div>
+                <div class="panel-heading">Facts</div>
                 <div class="panel-body">
                     <p>ID: <asp:Label ID="txtIMDB" runat="server" /></p>
-                    <p>Companies: ADD MOVIE COMPANIES</p>
                     <p>Budget: <asp:Label ID="txtBudget" runat="server" /></p>
                     <p>Revenue: <asp:Label ID="txtRevenue" runat="server" /></p>
                 </div>
@@ -109,12 +110,26 @@
              </div>
          </div>
         <!-- Add cast and crew members-->
+        <div class="panel-group" id="accordian" role="tablist">
         <div class="panel panel-default">
-         <div class="panel-heading">Cast</div>
-            <div class="panel-body">
-                <p>Add Cast Members</p>
+         <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
+             <a data-toggle="collapse" href="#collapseListGroup1" aria-expanded="true" aria-controls="collapseListGroup1">Cast</a>
+          </div>
+            <div class="panel-body"> 
+                <div id="collapseListGroup1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="collapseListGroupHeading1" aria-expanded="true">
+                    <asp:Repeater ID="rptCast" runat="server">
+                        <ItemTemplate>                          
+                             <ul class="list-group">
+                                 <li class="list-group-item"><img src="http://image.tmdb.org/t/p/w45/<%# Eval("profile_path") %>>" alt="" /> 
+                                     <a href="../SPerson/SPerson.aspx?id=<%# Eval("person_id") %>>"><%# Eval("name") %></a> <%# Eval("role_name") %></li>
+                                 
+                             </ul>
+                        </ItemTemplate>
+                    </asp:Repeater>
             </div>
+                 </div>
         </div>
+        </div><br />
 
          <div class="panel panel-default">
          <div class="panel-heading">Crew</div>
--- ASSIGN2/SMovie/SMovie.aspx.cs
+++ ASSIGN2/SMovie/SMovie.aspx.cs
@@ -21,8 +21,6 @@ public partial class SMovie : Page
 
     protected void DisplayMovieData(int rowCount, DataTable dt)
     {
-        if (rowCount > 0)
-        {
             DataRow row = dt.Rows[0];
             int content = (int)row["movie_id"];
             txtTitle.Text = (string)dt.Rows[0]["title"];
@@ -30,35 +28,64 @@ public partial class SMovie : Page
             txtRunTime.Text = Convert.ToInt32(dt.Rows[0]["runtime"]).ToString() + " Minutes";
             txtOverview.Text = (string)dt.Rows[0]["overview"];
             txtIMDB.Text = (string)dt.Rows[0]["imdb_id"];
-            txtBudget.Text = Convert.ToDecimal(dt.Rows[0]["budget"]).ToString();
-            txtRevenue.Text = Convert.ToDecimal(dt.Rows[0]["revenue"]).ToString();
+            txtBudget.Text = String.Format("{0:c}",dt.Rows[0]["budget"]).ToString();
+            txtRevenue.Text = String.Format("{0:c}",dt.Rows[0]["revenue"]).ToString();
             txtAverage.Text = Convert.ToDecimal(dt.Rows[0]["vote_average"]).ToString();
             txtVoteCount.Text = Convert.ToDecimal(dt.Rows[0]["vote_count"]).ToString();
             txtTagLine.Text = (string)dt.Rows[0]["tagline"];
-            //txtGenre.Text = (string)dt.Rows[0]["name"];
-        }
-        else
-        {
-            Response.Redirect("Error.aspx");
-        }
-
+            imgPoster.ImageUrl = "http://image.tmdb.org/t/p/w342/"+dt.Rows[0]["poster_path"];
     }
 
     protected void DisplayMovie()
     {
-        string movieID = Request["id"];
+        int movieID = 0;
         String connString = WebConfigurationManager.ConnectionStrings["Movies"].ConnectionString;
         OleDbConnection conn = new OleDbConnection(connString);
         try
         {
-           string sql = "SELECT * FROM movie as a";
+            if (Request.QueryString["id"] == null) { Response.Redirect("../Error.aspx"); }
+            else if (!Int32.TryParse(Request.QueryString["id"], out movieID)) { Response.Redirect("../Error.aspx"); }
+          
+            string sql = "SELECT * FROM movie as a";
            sql += " WHERE a.movie_id=" + movieID;
-            
+           
             OleDbDataAdapter adapter = new OleDbDataAdapter(sql, conn);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
             int rowCount = dt.Rows.Count;
             DisplayMovieData(rowCount, dt);
+
+            string sqlGenre = "SELECT genre.name FROM ((genre INNER JOIN movie_genre ON genre.genre_id = movie_genre.genre_id)"; 
+            sqlGenre += " INNER JOIN movie ON movie_genre.movie_id = movie.movie_id) WHERE (movie.movie_id = "+movieID+")";
+            OleDbDataAdapter adapterGenre = new OleDbDataAdapter(sqlGenre, conn);
+            DataTable dtG = new DataTable();
+            adapterGenre.Fill(dtG);
+            txtGenre.Text = tempLoop(dtG, "name");
+
+            string sqlKeyword = "SELECT keyword.name FROM ((movie_keyword INNER JOIN movie ON movie_keyword.movie_id = movie.movie_id)";
+            sqlKeyword += " INNER JOIN keyword ON movie_keyword.keyword_id = keyword.keyword_id) WHERE (movie.movie_id ="+movieID+")";
+            OleDbDataAdapter adapterKeyword = new OleDbDataAdapter(sqlKeyword, conn);
+            DataTable dtK = new DataTable();
+            adapterKeyword.Fill(dtK);
+            txtKeyword.Text = tempLoop(dtK, "name");
+
+            string sqlCompany = "SELECT company.company_name FROM ((movie_company INNER JOIN movie ON movie_company.movie_id = movie.movie_id)";
+            sqlCompany += " INNER JOIN company ON movie_company.company_id = company.company_id) WHERE (movie.movie_id =" + movieID + ")";
+            OleDbDataAdapter adapterCompany = new OleDbDataAdapter(sqlCompany, conn);
+            DataTable dtC = new DataTable();
+            adapterCompany.Fill(dtC);
+            txtCompany.Text = tempLoop(dtC, "company_name");
+
+            string sqlCast = "select person.name, person.person_id, movie_cast.ordering, movie_cast.role_name, person.profile_path";
+            sqlCast += " from person, movie_cast, movie";
+            sqlCast += " where movie.movie_id = movie_cast.movie_id and movie_cast.person_id = person.person_id";
+            sqlCast += " and movie.movie_id = "+movieID+" order by movie_cast.ordering";
+            OleDbDataAdapter adapterCast = new OleDbDataAdapter(sqlCast, conn);
+            DataTable dtCast = new DataTable();
+            adapterCast.Fill(dtCast);
+            rptCast.DataSource = dtCast;
+            rptCast.DataBind();
+
         }
         catch (Exception ex)
         {
@@ -70,4 +97,15 @@ public partial class SMovie : Page
             conn.Close();
         }
     }
+
+    protected string tempLoop(DataTable dt, string txtName) 
+    {
+        string output = " | ";
+        for (int num = 0; num < dt.Rows.Count; num++)
+        {
+            output += (string)dt.Rows[num][txtName]+" | ";
+        }
+        return output;
+    }
+
 }
